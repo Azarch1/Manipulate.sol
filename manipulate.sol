f30e53aa96947f3eda25f5b2033db672f787ebd0
SkynetDev <andrejohnson0017@gmail.com>
2020-12-23 18:51:07 -0500

    Implemented selfdestruct and pausing functions for the smart contract

--- manipulate.sol
+++ manipulate.sol
@@ -1,11 +1,29 @@
 pragma solidity ^0.7.4;
 
 contract StopStartPause {
+    address owner;
+
+    bool public paused;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+
     function sendMoney() public payable {
 
     }
     
+    function setPaused(bool _paused) public {
+        paused = _paused;
+    }
+
     function withdrawAllMoney(address payable _to) public {
+        require(msg.sender == owner,"You are not the owner");
+        require(!paused,"Contract is paused");
         _to.transfer(address(this).balance);
+    }
+
+    function destroySmartContract(address payable _to) public {
+        selfdestruct(_to);
     }
 }
